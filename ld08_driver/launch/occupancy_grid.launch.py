4af23eebd68da539d044a4c94088aed26acf84ae
David Michael Indraputra <144904094+MikejR2904@users.noreply.github.com>
2024-06-18 12:52:32 +0800

    Add files via upload

--- ld08_driver/launch/occupancy_grid.launch.py
+++ ld08_driver/launch/occupancy_grid.launch.py
@@ -23,10 +23,26 @@ from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+    ld08_cartographer_prefix = get_package_share_directory('ld08_driver')
+    configuration_directory = LaunchConfiguration('configuration_directory', default=os.path.join(
+                                                  ld08_cartographer_prefix, 'config'))
+    configuration_basename = LaunchConfiguration('configuration_basename',
+                                                 default='ld08_lds_2d.lua')
+    
     resolution = LaunchConfiguration('resolution', default='0.05')
     publish_period_sec = LaunchConfiguration('publish_period_sec', default='1.0')
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'configuration_directory',
+            default_value=configuration_directory,
+            description='Full path to config file to load'),
+        
+        DeclareLaunchArgument(
+            'configuration_basename',
+            default_value=configuration_basename,
+            description='Name of lua file for cartographer'),
+        
         DeclareLaunchArgument(
             'resolution',
             default_value=resolution,
@@ -42,20 +58,18 @@ def generate_launch_description():
             default_value='false',
             description='Use simulation (Gazebo) clock if true'),
 	
-	Node(
-	    package='cartographer_ros',
-	    executable='cartographer_node',
-	    name='cartographer_node',
-	    output='screen',
-	    parameters=[{'use_sim_time': use_sim_time}],
-	    remappings=[
-	    	('/submap_list', '/submap_slices'),
-	   	('/map', '/map'),
-	   	('/scan', '/scan'),
-	    ],
-	    arguments=['-configuration_directory', LaunchConfiguration('cartographer_config_dir', default=os.path.join(
-                                                  ld08_cartographer_prefix, 'config')), '-configuration_basename', LaunchConfiguration('configuration_basename',
-                                                 default='ld08_lds_2d.lua')],
+    	Node(
+            package='cartographer_ros',
+            executable='cartographer_node',
+            name='cartographer_node',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}],
+            remappings=[
+                ('/submap_list', '/submap_slices'),
+                ('/map', '/map'),
+                ('/scan', '/scan'),
+            ],
+            arguments=['-configuration_directory', configuration_directory, '-configuration_basename', configuration_basename],
             ),
                                                  
         Node(
